e509d4e8078050b4c65b18e64aa4faed10453475
KleberRFLeite <kleber_rfleite@hotmail.com>
2022-08-08 15:08:52 -0300

    Projecty hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/com/kleberworker/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kleberworker.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
