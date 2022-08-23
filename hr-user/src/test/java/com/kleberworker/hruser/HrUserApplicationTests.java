d992681ce7e9c50b4298fce7c2dd62f237cbdf20
KleberRFLeite <kleber_rfleite@hotmail.com>
2022-08-22 21:05:17 -0300

    Project Hr-User created

---
+++ hr-user/src/test/java/com/kleberworker/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kleberworker.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
