ee5a3f330ad0775bdbba1cae735a6c5270609643
KleberRFLeite <kleber_rfleite@hotmail.com>
2022-08-25 13:23:44 -0300

    correcao security server

--- hr-worker/src/main/java/com/kleberworker/hrworker/entities/Worker.java
+++ hr-worker/src/main/java/com/kleberworker/hrworker/entities/Worker.java
@@ -52,7 +52,10 @@ public class Worker implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -64,7 +67,12 @@ public class Worker implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Worker other = (Worker) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 	
